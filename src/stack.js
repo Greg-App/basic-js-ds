617040e16b24909d54b4dfc93e1c2b08693490c3
Greg App <greg358@mail.ru>
2022-10-16 19:43:19 +0300

    feat: add stack task

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class Stack {
+  constructor () {
+  this.stack=[];
   }
-
+  push(element) {
+   this.stack.push(element);
+  }
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.length>0?this.stack.pop():undefined;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.length>0?this.stack[this.stack.length-1]:undefined;
   }
 }
 
