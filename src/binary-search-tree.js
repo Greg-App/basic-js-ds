2277745e392a271171202d0966bf94217228f05c
Greg App <greg358@mail.ru>
2022-10-17 14:40:55 +0300

    feat:add Binary search tree task w_o remove method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,114 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+ const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor (value) {
+    this.rootT=value?value:null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.rootT?this.rootT:null;
+   
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const addedNode =new Node(data);
+    if(this.rootT===null) {
+      this.rootT=addedNode;
+    } else {this.addNode(this.rootT,addedNode);
+    }
+  }
+  
+  addNode(curNode,newNode) {
+    if (newNode.data>curNode.data) {
+      if(curNode.right===null) {
+       curNode.right=newNode;
+      } else {
+        this.addNode(curNode.right,newNode); 
+      }
+  }
+  else {
+    if(curNode.left===null) {
+      curNode.left=newNode;
+     } else {
+       this.addNode(curNode.left,newNode); 
+     }
+  }
+}
+
+
+  has(data) {
+    
+    if(!this.rootT) {
+      return false;
+    } else{
+      let current = this.rootT;
+      
+      while(current.data!==data) {
+        if(data>current.data) {
+          if(current.right===null) {
+            return false;
+          }
+          current=current.right;
+        } else {
+          if(current.left===null) {
+            return false;
+          }
+          current=current.left;
+        }
+      } 
+      return current.data===data?true:false;
+      
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    if(!this.rootT) {
+      return null;
+    } else{
+      let current = this.rootT;
+      
+      while(current.data!==data) {
+        if(data>current.data) {
+          if(current.right===null) {
+            return null;
+          }
+          current=current.right;
+        } else {
+          if(current.left===null) {
+            return null;
+          }
+          current=current.left;
+        }
+      } 
+      return current.data===data?current:null;
+      
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.rootT;
+    while(current.left!==null) {
+      current=current.left;
+    }
+    return current.data?current.data:null;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.rootT;
+    while(current.right!==null) {
+      current=current.right;
+    }
+    return current.data?current.data:null;
   }
 }
 
